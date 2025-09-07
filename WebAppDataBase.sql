43417958e05039b1ecc7df6a95dd560d65e46410
Jermaine Beea <jermainebeea@gmailcom>
2025-09-07 15:51:04 +0000

    STABLE VERSION [BASED ON MARKERT RATE]

--- WebAppDataBase.sql
+++ WebAppDataBase.sql
@@ -1,5 +1,5 @@
 -- WebAppDataBase Export
--- Generated on: Sun Sep 07 15:38:25 UTC 2025
+-- Generated on: Sun Sep 07 15:49:34 UTC 2025
 
 DROP TABLE IF EXISTS WebAppDataBase;
 
@@ -14,10 +14,10 @@ CREATE TABLE WebAppDataBase (
 );
 
 -- Insert data
-INSERT INTO WebAppDataBase (variable, maximum, minimum, factormin, factormax, returnmin, returnmax) VALUES ('tradeprofit', -88, -88, 0.0004972679, 0.0004972679, 88.0437680345, 88.0437680345);
+INSERT INTO WebAppDataBase (variable, maximum, minimum, factormin, factormax, returnmin, returnmax) VALUES ('tradeprofit', -88, -88, -0.0004970207, -0.0004970207, -88.0000000385, -88.0000000385);
 INSERT INTO WebAppDataBase (variable, maximum, minimum, factormin, factormax, returnmin, returnmax) VALUES ('profitfactor', -0.000497021, -0.000497021, -0.000497021, -0.000497021, -0.0004970207, -0.0004970207);
-INSERT INTO WebAppDataBase (variable, maximum, minimum, factormin, factormax, returnmin, returnmax) VALUES ('tradeamount', 10000, 10000, 9999.9939596627, 9999.9939596627, -9995.028832196, -9995.028832196);
-INSERT INTO WebAppDataBase (variable, maximum, minimum, factormin, factormax, returnmin, returnmax) VALUES ('buyvariable', 17.6967, 17.6967, 17.7143043813, 17.7143043813, 17.714304376, 17.714304376);
-INSERT INTO WebAppDataBase (variable, maximum, minimum, factormin, factormax, returnmin, returnmax) VALUES ('sellvariable', 17.7055, 17.7055, 17.6879043684693, 17.6879043684693, 17.6879043737783, 17.6879043737783);
+INSERT INTO WebAppDataBase (variable, maximum, minimum, factormin, factormax, returnmin, returnmax) VALUES ('tradeamount', 10000, 10000, 9999.9939596627, 9999.9939596627, 9999.999995625, 9999.999995625);
+INSERT INTO WebAppDataBase (variable, maximum, minimum, factormin, factormax, returnmin, returnmax) VALUES ('buyvariable', 17.7055, 17.7055, 17.7055000053, 17.7055000053, 17.7055, 17.7055);
+INSERT INTO WebAppDataBase (variable, maximum, minimum, factormin, factormax, returnmin, returnmax) VALUES ('sellvariable', 17.6967, 17.6967, 17.6966999946845, 17.6966999946845, 17.6966999999961, 17.6966999999961);
 
 -- End of export
