db27674d987307271324ff87402b5a66a6c8475f
Jermaine Beea <jermainebeea@gmailcom>
2025-10-10 10:26:04 +0200

    changes to default values

--- WebAppDataBase.sql
+++ WebAppDataBase.sql
@@ -1,5 +1,5 @@
 -- WebAppDataBase Export
--- Generated on: Tue Sep 09 12:46:38 SAST 2025
+-- Generated on: Fri Oct 10 09:50:03 SAST 2025
 
 DROP TABLE IF EXISTS WebAppDataBase;
 
@@ -12,10 +12,10 @@ CREATE TABLE WebAppDataBase (
 );
 
 -- Insert data
-INSERT INTO WebAppDataBase (variable, maximum, minimum, returnmin, returnmax) VALUES ('tradeprofit', -88, -88, -88.0000000385, -88.0000000385);
-INSERT INTO WebAppDataBase (variable, maximum, minimum, returnmin, returnmax) VALUES ('profitfactor', -0.000497021, -0.000497021, -0.0004970207, -0.0004970207);
-INSERT INTO WebAppDataBase (variable, maximum, minimum, returnmin, returnmax) VALUES ('tradeamount', 10000, 10000, 9999.999995625, 9999.999995625);
-INSERT INTO WebAppDataBase (variable, maximum, minimum, returnmin, returnmax) VALUES ('buyvariable', 17.7055, 17.7055, 17.7055, 17.7055);
-INSERT INTO WebAppDataBase (variable, maximum, minimum, returnmin, returnmax) VALUES ('sellvariable', 17.6967, 17.6967, 17.6966999999961, 17.6966999999961);
+INSERT INTO WebAppDataBase (variable, maximum, minimum, returnmin, returnmax) VALUES ('tradeprofit', 2000, 1500, 451.9999982034, 451.9999982034);
+INSERT INTO WebAppDataBase (variable, maximum, minimum, returnmin, returnmax) VALUES ('profitfactor', 0.04, 0.03, 0.0024357254, 0.0024357254);
+INSERT INTO WebAppDataBase (variable, maximum, minimum, returnmin, returnmax) VALUES ('tradeamount', 10000, 10000, 33185.840839871, 44247.7877864946);
+INSERT INTO WebAppDataBase (variable, maximum, minimum, returnmin, returnmax) VALUES ('buyvariable', 18.5571, 18.5571, 18.4531403235, 18.4039505743);
+INSERT INTO WebAppDataBase (variable, maximum, minimum, returnmin, returnmax) VALUES ('sellvariable', 18.6023, 18.6023, 18.7070999991574, 18.7571000007323);
 
 -- End of export
